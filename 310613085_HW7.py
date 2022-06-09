db671f2491ef7acb0e9e613775ed91bfc4ca446a
MarcoLei1228 <93649853+MarcoLei1228@users.noreply.github.com>
2022-06-09 18:15:35 +0800

    Update 310613085_HW7.py

--- 310613085_HW7.py
+++ 310613085_HW7.py
@@ -1,16 +1,10 @@
-from cgitb import grey
-from hashlib import new
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-land_shape = []
-land_size = sum(land_shape)
-land_num = 0
 
 global_loc = []
 global_island_loc_list = []
 
-def creat_new_world(): # 創建底圖
+def creat_new_world():                                 # 創建底圖
     column, row = 10, 10
     arr = np.zeros([10,10])
 
@@ -26,36 +20,31 @@ def creat_new_world(): # 創建底圖
     arr[9][6] = 1
     return arr
 
-def isLand(arr, x, y):
+def isLand(arr, x, y):                                 # 判斷是否土地
     if x >= 0 and x < 10 and y >= 0 and y < 10 and arr[x][y] == 1:
         return True
     return False
 
-def count_island(arr, x, y, tmp_island):
+def count_island(arr, x, y, tmp_island):               # 統計土地
     
     if isLand(arr, x, y) and ((x,y) not in global_loc):
         if (x,y) not in tmp_island :
 
             tmp_island.append((x,y))
-            print(f"({x},{y}) not in island!!")
             if (x, y) not in global_loc:
                 global_loc.append((x,y))
         
     
-            if count_island(arr, x+1, y, tmp_island):  # 向右行
-                print("right")
+            if count_island(arr, x+1, y, tmp_island):  # 向右走
                 return True
 
-            if count_island(arr, x, y+1, tmp_island):  # 向下行
-                print("down")
+            if count_island(arr, x, y+1, tmp_island):  # 向下走
                 return True
             
-            if count_island(arr, x-1, y, tmp_island):  # 向左行
-                print("left")
+            if count_island(arr, x-1, y, tmp_island):  # 向左走
                 return True
 
-            if count_island(arr, x, y-1, tmp_island):  # 向上行
-                print("top")
+            if count_island(arr, x, y-1, tmp_island):  # 向上走
                 return True
 
     if (tmp_island not in global_island_loc_list) and (tmp_island):
@@ -65,10 +54,10 @@ def count_island(arr, x, y, tmp_island):
 
 arr = creat_new_world()
 
-# Find Island
+# Find Island ()
 new_arr = np.zeros([10,10])
-for x in range(10):         # 直行
-    for y in range(10):     # 橫行
+for x in range(10):         
+    for y in range(10):     
         if isLand(arr, x, y):
             tmp_island = []
             count_island(arr, x, y, tmp_island)
@@ -78,12 +67,7 @@ for x in range(10):         # 直行
 for (x,y) in global_loc:
     new_arr[x,y] = 1
 
-# plt.figure(1)
-# plt.imshow(arr, cmap="gray_r")
-# plt.figure(1)
-# plt.imshow(new_arr, cmap="gray_r")
-# plt.show()
-
+# Ploting
 fig, axis = plt.subplots(1,5, figsize=(16,8))
 axis[0].imshow(arr, cmap='gray_r')
 axis[0].set_title("Ground Truth")
@@ -102,9 +86,4 @@ for idx, island_loc_list in enumerate(global_island_loc_list):
     axis[2+idx].set_title(f"Islands {idx+1} size: {len(island_loc_list)}")
     axis[2+idx].set_xticks(np.arange(0,10))
     axis[2+idx].set_yticks(np.arange(0,10))
-
 plt.show()
-
-
-for i in range(len(global_island_loc_list)):
-    print(f"{i}, {global_island_loc_list[i]}, size={len(global_island_loc_list[i])}")
